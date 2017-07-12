c3e9b687fd83c4786c8b6d443be8e86d03bac3bc
Grzegorz Kowalski <g.kowalski@uj.edu.pl>
2017-07-12 15:38:31 +0200

    return empty command/attribute/attrinfo list on error

--- src/tangojs-connector-mtango/MTangoConnector.js
+++ src/tangojs-connector-mtango/MTangoConnector.js
@@ -1,7 +1,11 @@
-
 import * as tangojs from 'tangojs-core'
 import * as fetchFn from 'node-fetch'
 import * as btoaFn from 'btoa'
+
+function none()
+{
+  return []
+}
 
 /**
  * @param {string} type
@@ -225,6 +229,7 @@ export class MTangoConnector extends tangojs.Connector {
 
     return this._fetch('get', `devices/${devname}/attributes`)
       .then(attributes => attributes.map(a => a.name))
+      .catch(none)
   }
 
   /**
@@ -251,6 +256,7 @@ export class MTangoConnector extends tangojs.Connector {
           event_prop: new tangojs.tango.EventProperties(info.event_prop)
         }))
       }))
+      .catch(none)
   }
 
   /**
@@ -356,6 +362,7 @@ export class MTangoConnector extends tangojs.Connector {
           }))
         })
       })
+      .catch(none)
   }
 
 }
